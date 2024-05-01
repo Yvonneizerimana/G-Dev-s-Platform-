79a5bfa4efc6cfd42ddd3df741eab38b5858db23
yvonne IZERIMANA <izerimanayvonne510@gmail.com>
2024-05-01 16:41:07 +0200

    only pdf file is allowed

--- W.Devs/utils/uploadDocument.js
+++ W.Devs/utils/uploadDocument.js
@@ -1,4 +1,6 @@
 import multer from 'multer';
+import path from 'path';
+
 const storage=multer.diskStorage({
       destination:(req,file,cb)=>{
         cb(null,"W.Devs/files")
@@ -7,8 +9,19 @@ const storage=multer.diskStorage({
   cb(null,file.originalname);
       }
 });
+const fileFilter = (req, file, cb) => {
+  const fileExtension = path.extname(file.originalname).toLowerCase();
+  if (fileExtension === '.pdf') {
+      cb(null, true); // Accept the file
+  } else {
+      cb(new Error('Only PDF files are allowed')); // Reject the file
+  }
+};
 
-const upload=multer({storage:storage})
+// Set up multer middleware with file filter
+const upload = multer({ 
+  storage: storage,
+  fileFilter: fileFilter});
 
 export default upload
 
